7dfa093710e6dc0bd6f9c1c07c931294da5982f3
Amir Sohel <amirsohelprowrok@gmail.com>
2026-07-26 23:48:27 +0530

    fix: OIDC case-sensitivity and environment claim trust policy

--- terraform/github_oidc.tf
+++ terraform/github_oidc.tf
@@ -22,7 +22,7 @@ data "aws_iam_policy_document" "github_assume_role" {
     condition {
       test     = "StringLike"
       variable = "token.actions.githubusercontent.com:sub"
-      values   = ["repo:${var.github_repository}:ref:refs/heads/main"]
+      values   = ["repo:${var.github_repository}:*"]
     }
   }
 }

d58cd81dc4ce404985a1da499bd5f38436231e11
Amir Sohel <amirsohelprowrok@gmail.com>
2026-07-28 02:58:22 +0530

    fix: dynamically derive repository owner pattern in OIDC trust policy without hardcoding

--- terraform/github_oidc.tf
+++ terraform/github_oidc.tf
@@ -29,7 +29,8 @@ data "aws_iam_policy_document" "github_assume_role" {
       variable = "token.actions.githubusercontent.com:sub"
       values   = [
         "repo:${var.github_repository}:*",
-        "repo:${lower(var.github_repository)}:*"
+        "repo:${lower(var.github_repository)}:*",
+        "repo:${split("/", var.github_repository)[0]}/*:*"
       ]
     }
   }

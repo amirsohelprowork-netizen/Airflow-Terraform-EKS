ce701e2ea569f69984d419b09e102a99cf997b92
Amir Sohel <amirsohelprowrok@gmail.com>
2026-07-28 17:33:56 +0530

    fix: dynamically fetch GitHub OIDC thumbprint

--- terraform/github_oidc.tf
+++ terraform/github_oidc.tf
@@ -1,15 +1,13 @@
 data "aws_caller_identity" "current" {}
 
+data "tls_certificate" "github" {
+  url = "https://token.actions.githubusercontent.com/.well-known/openid-configuration"
+}
+
 resource "aws_iam_openid_connect_provider" "github" {
   url            = "https://token.actions.githubusercontent.com"
   client_id_list = ["sts.amazonaws.com"]
-  # Note: As of mid-2023, AWS manages the certificates for GitHub OIDC automatically.
-  # These thumbprints are kept purely to satisfy the Terraform API requirements,
-  # but AWS will ignore them and use its own trusted Root CAs. You do not need to rotate these.
-  thumbprint_list = [
-    "6938fd4d98bab03faadb97b34396831e3780aea1",
-    "1c58a3a8518e8759bf075b76b750d4f2df264fcd"
-  ]
+  thumbprint_list = [data.tls_certificate.github.certificates[0].sha1_fingerprint]
 }
 
 data "aws_iam_policy_document" "github_assume_role" {

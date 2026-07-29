18422359e673d68fd0e66d76ff4547cceaf2b651
Amir Sohel <amirsohelprowrok@gmail.com>
2026-07-29 22:50:35 +0530

    diagrams

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,9 +1,9 @@
-terraform {
+﻿terraform {
   backend "s3" {
-    bucket         = "airflow-enterprise-demo-tfstate-20260727235127"
-    key            = "terraform.tfstate"
+    bucket         = "airflow-eks-tfstate-20260728231302"
+    key            = "airflow-demo/terraform.tfstate"
     region         = "us-east-1"
-    dynamodb_table = "airflow-enterprise-demo-tflock"
+    dynamodb_table = "airflow-eks-tflock-20260728231302"
     encrypt        = true
   }
 }
